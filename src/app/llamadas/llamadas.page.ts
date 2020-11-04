989dd1e39b80238e702dbfd77af5c74bec85fd9b
daniblillo <daniel.10.betico@gmail.com>
2020-11-04 23:06:36 +0100

    Copia seguridad

--- src/app/llamadas/llamadas.page.ts
+++ src/app/llamadas/llamadas.page.ts
@@ -14,6 +14,24 @@ export class LlamadasPage implements OnInit {
       title: 'Han Solo',
       comments: ['Cazarrecompensas'],
       imageURL: '/assets/icon/hansolo.png'
+    },
+    {
+      id: '2',
+      title: 'Rey',
+      comments: ['Recolectora'],
+      imageURL: '/assets/icon/rey.png'
+    },
+    {
+      id: '3',
+      title: 'Finn',
+      comments: ['Piloto'],
+      imageURL: '/assets/icon/finn.png'
+    },
+    {
+      id: '4',
+      title: 'Leia',
+      comments: ['Princesa'],
+      imageURL: '/assets/icon/leia.png'
     }
 
   ]
